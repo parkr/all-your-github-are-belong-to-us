1f6e6fe6b95f0f4056d234e218ec3c3af46bba8c
Parker Moore <parkrmoore@gmail.com>
2014-02-17 01:00:46 -0500

    Add initial stuff

---
+++ index.js
@@ -0,0 +1,33 @@
+(function(){
+  "use strict";
+  
+  var github = require('git-at-me');
+
+  github({
+    token: require('./github-token'),
+    user: process.env.GITHUB_USERNAME,
+    repo: process.env.GITHUB_REPOSITORY,
+    // http://developer.github.com/v3/repos/hooks/#events
+    events: JSON.parse(process.env.GITHUB_HOOK_EVENTS),
+    // The URL that github will post to; should match your site url
+    url: process.env.AYGABTU_URL + '/git-at-me/events',
+    // More options for configuration documented below
+    port: process.env.PORT
+  }).on('push', function (pushInfo) {
+    // Do something with commits
+  }).on('pull_request', function (prInfo) {
+    // Do something with PR
+  }).on('issue', function (issueInfo) {
+    // Do something with issue
+  }).on('release', function (releaseInfo) {
+    // Do something with release
+  });
+
+  /* **Snip all the express configuration** */
+
+  server.get('index', function (req, res) {
+    res.render('index'); 
+  });
+
+  server.listen();
+})();
